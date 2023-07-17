3467976965727f4d6bebaec7eac55b37590441f3
Dong <3400772707@qq.com>
2023-07-17 19:07:57 +0800

    swap function

--- test.c
+++ test.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+void swap(*a,*b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(){
     int x = 2;
     int y = 3;
-    int temp = x;
-    x = y;
-    y = temp;
+    swap(&x,&y);
     pirntf("%d,%d",x,y);
 }
