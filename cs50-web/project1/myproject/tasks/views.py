d29bd451c169d94fce9d4279d12e1cf6f47f881f
Dong <3400772707@qq.com>
2023-07-28 09:57:24 +0800

    begin project1

--- cs50-web/project1/myproject/tasks/views.py
+++ cs50-web/project1/myproject/tasks/views.py
@@ -1,3 +1,36 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="new task:")
+
+
+def index(request):
+    if request.session.get("tasks") == None:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session['tasks']
+    })
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            print(type(request.session.get('tasks')))
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    else:
+        return render(request, "tasks/add.html", {
+            "form": NewTaskForm()
+        })
